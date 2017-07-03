2ef11ec54b5a333027b1b9e5b959195cc92e80a5
Administrator <2771947038@qq.com>
2017-07-03 13:04:23 +0800

    生产者与消费者

---
+++ workprocude/src/test/java/com/ljl/ThreadLocalTest.java
@@ -0,0 +1,41 @@
+package com.ljl;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Administrator on 2017/6/27.
+ */
+public class ThreadLocalTest
+{
+    public static void main(String[] args) {
+        ThreadLocal local = new ThreadLocal();
+        Map m = new HashMap();
+        m.put("1","1");
+        local.set(m);
+        Run1 run1 = new Run1(local);
+        run1.start();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(local.get());
+
+    }
+}
+class Run1 extends Thread{
+    public Run1(ThreadLocal t) {
+        this.t = t;
+    }
+
+    private ThreadLocal t;
+
+    @Override
+    public void run() {
+        System.out.println(this.getName()+":"+t.get());
+        Map m = new HashMap();
+        m.put("2","2");
+        t.set(m);
+    }
+}
