183049a79100b58a57951f3957f5057d0ee2c3a1
qq57694878 <57694878@qq.com>
2017-09-30 13:06:58 +0800

    tijiao20170930

---
+++ solrdemo/src/main/java/cn/com/jldata/solrdemo/config/WebConfig.java
@@ -0,0 +1,73 @@
+package cn.com.jldata.solrdemo.config;
+
+
+import cn.com.jldata.solrdemo.converter.DateConverter;
+import cn.com.jldata.solrdemo.converter.JsonMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.*;
+
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
+
+/*@Configuration
+@EnableWebMvc // 启用 SpringMVC ，相当于 xml中的 <mvc:annotation-driven/>
+@ComponentScan(basePackages = {"cn.com.jldata.solrdemo.**.controller"},
+        includeFilters = @ComponentScan.Filter(classes = Controller.class),
+        useDefaultFilters = false)*/
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    /**
+     * 设置由 web容器处理静态资源 ，相当于 xml中的<mvc:default-servlet-handler/>
+     */
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Override
+    public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
+        configurer.setDefaultTimeout(3000);
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new DateConverter());
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
+        List<MediaType> supportedMediaTypes = new ArrayList<MediaType>();
+        supportedMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
+        supportedMediaTypes.add(MediaType.APPLICATION_FORM_URLENCODED);
+        supportedMediaTypes.add(MediaType.TEXT_PLAIN);
+        supportedMediaTypes.add(MediaType.TEXT_HTML);
+        jsonConverter.setSupportedMediaTypes(supportedMediaTypes);
+        jsonConverter.setDefaultCharset(Charset.forName("UTF-8"));
+        jsonConverter.setObjectMapper(new JsonMapper());
+        converters.add(jsonConverter);
+    }
+
+
+
+    /**
+     * 配置跨域
+     * @param registry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowCredentials(true).allowedOrigins("*").allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+                .maxAge(1800);
+    }
+
+}
