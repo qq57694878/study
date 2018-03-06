f79ecaabbb25c8c711f91703ef51836dab3a3ee9
qq57694878 <57694878@qq.com>
2018-03-06 16:46:16 +0800

    备忘

---
+++ vjspdemo/src/main/java/com/ljl/vuehttp/encryption/MD5.java
@@ -0,0 +1,11 @@
+package com.ljl.vuehttp.encryption;
+
+import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
+
+public class MD5 {
+    public static void main(String[] args) {
+        Md5PasswordEncoder md5PasswordEncoder = new Md5PasswordEncoder();
+        String result = md5PasswordEncoder.encodePassword("123","1");
+        System.out.println(result);
+    }
+}
