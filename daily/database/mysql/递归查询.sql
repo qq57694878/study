c4eceed4bc78b90b3260edbbd5a5a241e1644ffb
lijinliang <57694878@qq.com>
2018-12-24 16:04:41 +0800

    修改

--- daily/database/mysql/递归查询.sql
+++ daily/database/mysql/递归查询.sql
@@ -14,3 +14,19 @@ SELECT GROUP_CONCAT(id) INTO sTempChd FROM t_areainfo WHERE FIND_IN_SET(parentId
 END WHILE;
 RETURN sTemp;
 END;
+
+
+
+SELECT CATALOG_CODE,CATALOG_NAME,CATALOG_LEVEL,CATALOG_PID
+FROM (
+       SELECT
+         @r AS _id,
+         (SELECT @r := CATALOG_PID FROM gs_sku_catalog WHERE id = _id) AS parent_id,
+         @l := @l + 1 AS lvl
+       FROM
+         (SELECT @r := 11, @l := 0) vars,
+         gs_sku_catalog h
+       WHERE @r <> 0) T1
+  JOIN gs_sku_catalog T2
+    ON T1._id = T2.id
+ORDER BY id;
