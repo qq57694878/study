15fce70b159525797b9244d0edf7ba5d7cc199cd
qq57694878 <57694878@qq.com>
2019-02-26 17:20:40 +0800

    设计模式添加

--- springstudy/base/src/main/java/TTT.java
+++ springstudy/base/src/main/java/TTT.java
@@ -1,5 +1,31 @@
+import java.util.*;
+
 /**
  * Created by Administrator on 2017/8/14/014.
  */
 public class TTT {
+	public static void main(String[] args) {
+		final Map m = new HashMap();
+		m.put("key","value");
+		System.out.println(m);
+		List<String> list = new ArrayList<String>();
+		list.add("1");
+		list.add("2");
+		list.add("3");
+		list.add("4");
+//		for (int i = 0; i < list.size(); i++) {
+//			System.out.println(list.get(i));
+//			list.remove(i);
+//		}
+		Iterator<String> iter =  list.iterator();
+		while(iter.hasNext()){
+			//System.out.println(iter.next());
+			//iter.next();
+			//iter.remove();
+		//	list.remove(iter.next());
+		}
+				for (int i = 0; i < list.size(); i++) {
+					System.out.println(list.get(i));
+				}
+	}
 }
